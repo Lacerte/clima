c148c4acc8d7801efa062c347744c569c9f61116
PrestoSole <yalather70@pm.me>
2020-12-27 21:46:26 +0300

    Removed the get current location's button

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -13,7 +13,9 @@ class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.black),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
       body: Container(
         color: Colors.black,
         constraints: const BoxConstraints.expand(),
