ee4a66fd96881081d8a130bc47222e9d5ce0c07d
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-01-14 10:55:43 +0300

    Revert "Elaborate on Architecture (#20)" (#21)
    This reverts commit a0d3fd34da59cdaa1e77da1d0ab182036ae0c1b3.

---
+++ packages/clima_domain/lib/clima_domain.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library clima_domain;
+
+export 'src/clima_domain_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
