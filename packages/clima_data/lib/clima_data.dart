98ae219263ae6cb17c159739d7b448f2be0f7f4f
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-01-14 10:21:43 +0300

    Elaborate on Architecture (#1)

--- packages/clima_data/lib/clima_data.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library clima_data;
-
-export 'src/clima_data_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
--- packages/clima_domain/lib/clima_domain.dart
+++
@@ -1,8 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library clima_domain;
-
-export 'src/clima_domain_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
