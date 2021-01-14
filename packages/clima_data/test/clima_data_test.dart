50f552ad42952486fbf0d25e1069181620f24597
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-01-14 10:22:20 +0300

    Revert "Elaborate on Architecture (#1)" (#2)
    This reverts commit 98ae219263ae6cb17c159739d7b448f2be0f7f4f.

---
+++ packages/clima_data/test/clima_data_test.dart
@@ -0,0 +1,16 @@
+import 'package:clima_data/clima_data.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
