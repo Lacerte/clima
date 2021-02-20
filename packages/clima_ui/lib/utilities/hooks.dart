32121610be9c6c2c1164a53f1552856b997bc8bd
Mohammed Anas <6daf084a-8eaf-40fb-86c7-8500077c3b69@anonaddy.me>
2021-02-20 13:27:37 +0000

    Create useGlobalKey and use it in screens (#44)
    * Create useGlobalKey and use it in screens
    
    * Add missing const

--- packages/clima_ui/lib/utilities/hooks.dart
+++ packages/clima_ui/lib/utilities/hooks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter/widgets.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 /// Creates a [FloatingSearchBarController].
@@ -12,3 +13,9 @@ FloatingSearchBarController useFloatingSearchBarController() {
 
   return controller;
 }
+
+/// Creates a [GlobalKey] that stays the same tilll the widget's lifetime ends.
+GlobalKey<T> useGlobalKey<T extends State<StatefulWidget>>() =>
+    // An empty list is given to `useMemoized` so that the global key will
+    // never be reset. See `useMemoized`'s documentation.
+    useMemoized(() => GlobalKey<T>(), const []);
