984068117da776c19cf2d28b70467db97ad6deb0
PrestoSole <yalather70@pm.me>
2021-04-17 17:14:01 +0300

    Removed the resuable widget because it's part of the old app design and it is no longer needed

--- packages/clima_ui/lib/utilities/reusable_widgets.dart
+++ packages/clima_ui/lib/utilities/reusable_widgets.dart
@@ -1,35 +1,5 @@
 import 'package:clima_core/failure.dart';
-import 'package:clima_ui/main.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/all.dart';
-
-class ReusableWidgets extends StatelessWidget {
-  const ReusableWidgets({this.cardChild});
-
-  final Widget cardChild;
-
-  @override
-  Widget build(BuildContext context) {
-    final _themeState = context.read(themeStateNotifier);
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: _themeState.isDarkTheme
-              ? const Color(0xFF171717)
-              : const Color(0xFFFCFCFC),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: cardChild,
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 SnackBar snackBar({String text, SnackBarAction action, int duration}) {
   return SnackBar(
