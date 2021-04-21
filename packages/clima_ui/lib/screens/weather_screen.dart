d3c464a363291aa567d8cd568832beb2aad27a10
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-04-21 17:37:04 +0300

    Extract weather_screen body (#77)

--- packages/clima_ui/lib/screens/weather_screen.dart
+++ packages/clima_ui/lib/screens/weather_screen.dart
@@ -6,9 +6,7 @@ import 'package:clima_ui/state_notifiers/forecasts_state_notifier.dart' as f;
 import 'package:clima_ui/state_notifiers/weather_state_notifier.dart' as w;
 import 'package:clima_ui/utilities/hooks.dart';
 import 'package:clima_ui/utilities/reusable_widgets.dart';
-import 'package:clima_ui/widgets/current_conditions_widgets.dart';
-import 'package:clima_ui/widgets/forecast_widget.dart';
-import 'package:clima_ui/widgets/value_tile.dart';
+import 'package:clima_ui/widgets/weather_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -40,8 +38,6 @@ class LocationScreen extends HookWidget {
     final isLoading = useState(weatherState is c.Loading);
 
     final cityStateNotifier = useProvider(c.cityStateNotifierProvider);
-
-    final weather = useProvider(w.weatherStateNotifierProvider.state).weather;
 
     void showFailureSnackBar(
         {@required Failure failure, VoidCallback onRetry, int duration}) {
@@ -207,133 +203,7 @@ class LocationScreen extends HookWidget {
             showIfClosed: false,
           )
         ],
-        body: () {
-          if (weather == null) {
-            return const SizedBox.shrink();
-          }
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Flexible(
-                flex: 10,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16),
-                      child: Text(
-                        weather.cityName.toUpperCase(),
-                        style: TextStyle(
-                          fontWeight: FontWeight.w900,
-                          letterSpacing: 5,
-                          color: Theme.of(context).textTheme.subtitle1.color,
-                          fontSize: 25,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      weather.description.toUpperCase(),
-                      style: TextStyle(
-                        fontWeight: FontWeight.w100,
-                        letterSpacing: 5,
-                        fontSize: 15,
-                        color: Theme.of(context).textTheme.subtitle1.color,
-                      ),
-                    ),
-                    const CurrentConditions(),
-                  ],
-                ),
-              ),
-              Divider(
-                color:
-                    Theme.of(context).textTheme.subtitle1.color.withAlpha(65),
-              ),
-              Flexible(
-                flex: 2,
-                child: ForecastHorizontal(),
-              ),
-              Divider(
-                color:
-                    Theme.of(context).textTheme.subtitle1.color.withAlpha(65),
-              ),
-              Flexible(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      ValueTile(
-                          'wind speed', '${weather.windSpeed.round()} m/s'),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Center(
-                          child: Container(
-                            width: 1,
-                            height: 35,
-                            color: Theme.of(context)
-                                .textTheme
-                                .subtitle1
-                                .color
-                                .withAlpha(65),
-                          ),
-                        ),
-                      ),
-                      ValueTile(
-                        'sunrise',
-                        DateFormat('h:mm a').format(
-                          DateTime.fromMillisecondsSinceEpoch(
-                            weather.sunrise * 1000,
-                          ).toUtc().add(
-                                weather.timeZoneOffset,
-                              ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Center(
-                          child: Container(
-                            width: 1,
-                            height: 35,
-                            color: Theme.of(context)
-                                .textTheme
-                                .subtitle1
-                                .color
-                                .withAlpha(65),
-                          ),
-                        ),
-                      ),
-                      ValueTile(
-                        'sunset',
-                        DateFormat('h:mm a').format(
-                          DateTime.fromMillisecondsSinceEpoch(
-                                  weather.sunset * 1000)
-                              .toUtc()
-                              .add(weather.timeZoneOffset),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Center(
-                          child: Container(
-                            width: 1,
-                            height: 35,
-                            color: Theme.of(context)
-                                .textTheme
-                                .subtitle1
-                                .color
-                                .withAlpha(65),
-                          ),
-                        ),
-                      ),
-                      ValueTile('humidity', '${weather.humidity}%'),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          );
-        }(),
+        body: const WeatherWidget(),
       ),
     );
   }
