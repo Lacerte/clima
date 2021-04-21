1cf918162d4e3eb006eab4c2d735656a339f9d62
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-04-21 17:34:26 +0300

    Move the getIconData function to a seperate file under the utilities dir (#80)

--- packages/clima_ui/lib/widgets/current_conditions_widgets.dart
+++ packages/clima_ui/lib/widgets/current_conditions_widgets.dart
@@ -1,5 +1,5 @@
-import 'package:clima_ui/screens/weather_screen.dart';
 import 'package:clima_ui/state_notifiers/weather_state_notifier.dart';
+import 'package:clima_ui/utilities/weather_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
