b0c141cb6129ac65d35f70e80dedf633b4cdfc41
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-04-30 14:28:52 +0300

    Settings Page (#85)

---
+++ packages/clima_ui/lib/screens/about_screen.dart
@@ -0,0 +1,84 @@
+import 'package:clima_ui/widgets/reusable_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:package_info/package_info.dart';
+
+class AboutScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        title: Text(
+          'About Clima',
+          style: Theme.of(context).appBarTheme.textTheme.subtitle1,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const SettingsHeader(
+              title: 'Information',
+            ),
+            const SettingsTile(
+              title: 'Changelog',
+              subtitle: 'Version 2.0',
+              icon: Icon(Icons.new_releases_outlined),
+            ),
+            SettingsTile(
+              title: 'Libraries',
+              subtitle: 'Open-source libraries used in the app',
+              icon: const Icon(Icons.source_outlined),
+              onTap: () async {
+                final PackageInfo packageInfo =
+                    await PackageInfo.fromPlatform();
+                showLicensePage(
+                  context: context,
+                  applicationName: packageInfo.appName,
+                  applicationVersion: packageInfo.version,
+                );
+              },
+            ),
+            SettingsTile(
+              title: 'Feedback',
+              subtitle: 'Bugs and feature requests',
+              icon: const Icon(Icons.help_outline),
+              onTap: () {
+                showGeneralSheet(
+                  context,
+                  title: 'Feedback',
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: const [
+                      SettingsTile(
+                        icon: Icon(Icons.bug_report_outlined),
+                        title: 'Submit issue',
+                      ),
+                      SettingsTile(
+                        icon: Icon(Icons.email_outlined),
+                        title: 'Contact developer',
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            const SettingsTile(
+              title: 'Source code',
+              subtitle: 'Clima is free software licensed under the GPLv3',
+              icon: Icon(FontAwesomeIcons.github),
+              isThreeLine: true,
+            ),
+            const SettingsDivider(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ packages/clima_ui/lib/widgets/unit_dialog.dart
@@ -0,0 +1,48 @@
+import 'package:clima_domain/entities/unit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UnitDialog extends ConsumerWidget {
+  static const _dialogOptions = {
+    'Metric': UnitSystem.metric,
+    'Imperial': UnitSystem.imperial,
+  };
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    final unit = watch(unitProvider);
+
+    final radios = [
+      for (final entry in _dialogOptions.entries)
+        RadioListTile<UnitSystem>(
+          title: Text(
+            entry.key.toString(),
+            style: TextStyle(
+              color: Theme.of(context).textTheme.subtitle1.color,
+            ),
+          ),
+          value: entry.value,
+          groupValue: unit.state,
+          onChanged: (newValue) {
+            unit.state = newValue;
+            Navigator.pop(context);
+          },
+        )
+    ];
+
+    return SimpleDialog(
+      title: Text(
+        'Unit',
+        style: TextStyle(
+          color: Theme.of(context).textTheme.subtitle1.color,
+        ),
+      ),
+      children: [
+        Column(
+          mainAxisSize: MainAxisSize.min,
+          children: radios,
+        ),
+      ],
+    );
+  }
+}
