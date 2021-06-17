9fcf4cd9bfcd65e4cd95e4378642e990f7ddc952
PrestoSole <yalather70@pm.me>
2021-06-17 23:48:20 +0300

    Color improvements

--- packages/clima_ui/lib/themes/dark_theme.dart
+++ packages/clima_ui/lib/themes/dark_theme.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 ThemeData darkTheme = ThemeData.dark().copyWith(
-  iconTheme: const IconThemeData(color: Color(0xFFF2F3F5)),
+  iconTheme: const IconThemeData(color: Color(0xFF9BA0A6)),
   snackBarTheme: const SnackBarThemeData(backgroundColor: Color(0xFFE6E6E6)),
   popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF36373B)),
-  dialogBackgroundColor: const Color(0xFF36373B),
+  dialogBackgroundColor: const Color(0xFF202125),
   toggleableActiveColor: const Color(0xFF89B4F8),
   accentColor: const Color(0xFF89B4F8),
   primaryColor: const Color(0xFF202125),
@@ -15,9 +15,9 @@ ThemeData darkTheme = ThemeData.dark().copyWith(
     subtitle2: TextStyle(color: Color(0xFF9BA0A6)),
   ),
   appBarTheme: const AppBarTheme(
-    iconTheme: IconThemeData(color: Color(0xFFF2F3F5)),
+    iconTheme: IconThemeData(color: Color(0xFF9BA0A6)),
     textTheme: TextTheme(subtitle1: TextStyle(color: Color(0xFFE9EAEE))),
-    actionsIconTheme: IconThemeData(color: Color(0xFFF2F3F5)),
+    actionsIconTheme: IconThemeData(color: Color(0xFF9BA0A6)),
     color: Color(0xFF202125),
     brightness: Brightness.dark,
   ),
