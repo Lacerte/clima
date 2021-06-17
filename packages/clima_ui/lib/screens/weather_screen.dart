345b28038640d33c21d1ae5a2e445b9b745c122d
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-06-17 22:36:55 +0300

    Update weather_screen.dart

--- packages/clima_ui/lib/screens/weather_screen.dart
+++ packages/clima_ui/lib/screens/weather_screen.dart
@@ -6,7 +6,7 @@ import 'package:clima_ui/state_notifiers/weather_state_notifier.dart' as w;
 import 'package:clima_ui/utilities/constants.dart';
 import 'package:clima_ui/utilities/failure_snack_bar.dart';
 import 'package:clima_ui/utilities/hooks.dart';
-import 'package:clima_ui/widgets/bottom_row';
+import 'package:clima_ui/widgets/bottom_row.dart';
 import 'package:clima_ui/widgets/current_conditions.dart';
 import 'package:clima_ui/widgets/forecast_widget.dart';
 import 'package:clima_ui/widgets/reusable_widgets.dart';
