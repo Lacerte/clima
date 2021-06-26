2bcacfe8afe3088e6856099c5a4efdbe654ca027
PrestoSole <yalather70@pm.me>
2021-06-26 15:11:49 +0300

    .

--- packages/clima_ui/lib/themes/dark_theme.dart
+++ packages/clima_ui/lib/themes/dark_theme.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 ThemeData darkTheme = ThemeData.dark().copyWith(
   iconTheme: const IconThemeData(color: Color(0xFF9BA0A6)),
   snackBarTheme: const SnackBarThemeData(backgroundColor: Color(0xFF2D2E30)),
-  popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF202125)),
+  popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF36373B)),
   dialogBackgroundColor: const Color(0xFF202125),
   toggleableActiveColor: const Color(0xFF89B4F8),
   accentColor: const Color(0xFF89B4F8),
@@ -16,9 +16,9 @@ ThemeData darkTheme = ThemeData.dark().copyWith(
   ),
   appBarTheme: const AppBarTheme(
     iconTheme: IconThemeData(color: Color(0xFF9BA0A6)),
-    textTheme: TextTheme(subtitle1: TextStyle(color: Color(0xFFFFFFFF))),
+    textTheme: TextTheme(subtitle1: TextStyle(color: Color(0xFFE9EAEE))),
     actionsIconTheme: IconThemeData(color: Color(0xFF9BA0A6)),
-    color: Color(0xFF000000),
+    color: Color(0xFF202125),
     brightness: Brightness.dark,
   ),
 );
