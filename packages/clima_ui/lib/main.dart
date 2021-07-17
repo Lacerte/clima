8f013828bdab62eb510b6293dad87dd7b7b3826d
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-07-17 23:36:24 +0300

    Update main.dart

--- packages/clima_ui/lib/main.dart
+++ packages/clima_ui/lib/main.dart
@@ -26,7 +26,7 @@ Future<void> main() async {
   // before running `runApp` throws an error.
   WidgetsFlutterBinding.ensureInitialized();
 
-  // The following line is because iOS doesn't return back
+  // The following line is added because iOS doesn't return back
   // the status bar and navigation bar on it's own.
   SystemChrome.setEnabledSystemUIOverlays(
       [SystemUiOverlay.bottom, SystemUiOverlay.top]);
