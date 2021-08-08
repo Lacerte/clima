c5aff4173b4fa514c774f4ee62e10900edd0262d
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-08-08 14:02:15 +0300

    Update black_theme.dart

--- packages/clima_ui/lib/themes/black_theme.dart
+++ packages/clima_ui/lib/themes/black_theme.dart
@@ -8,16 +8,16 @@ ThemeData blackTheme = ThemeData.dark().copyWith(
   // explicitly.
   brightness: Brightness.dark,
   iconTheme: const IconThemeData(color: Color(0xFF9BA0A6)),
-  snackBarTheme: const SnackBarThemeData(
-    backgroundColor: Color(0xFF202125),
-    contentTextStyle: TextStyle(color: Color(0xFFE9EAEE)),
-  ),
   popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF202125)),
   dialogBackgroundColor: const Color(0xFF202125),
   toggleableActiveColor: const Color(0xFF89B4F8),
   accentColor: const Color(0xFF89B4F8),
   primaryColor: const Color(0xFF000000),
   scaffoldBackgroundColor: const Color(0xFF000000),
+  snackBarTheme: const SnackBarThemeData(
+    backgroundColor: Color(0xFF202125),
+    contentTextStyle: TextStyle(color: Color(0xFFE9EAEE)),
+  ),
   textTheme: const TextTheme(
     subtitle1: TextStyle(color: Color(0xFFE9EAEE)),
     subtitle2: TextStyle(color: Color(0xFF9BA0A6)),
