523ae70917b720d4d4c09f96ddbee28b9fd4e379
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2021-08-31 11:50:13 +0300

    Remove unrelated changes

--- packages/clima_ui/lib/themes/light_theme.dart
+++ packages/clima_ui/lib/themes/light_theme.dart
@@ -10,7 +10,6 @@ ThemeData lightTheme = ThemeData.light().copyWith(
   iconTheme: const IconThemeData(color: Color(0xFF5F6267)),
   toggleableActiveColor: const Color(0xFF1A73E9),
   accentColor: const Color(0xFF1A73E9),
-  colorScheme: ColorScheme.fromSwatch(accentColor: const Color(0xFF1A73E9)),
   primaryColor: const Color(0xFFFFFFFF),
   scaffoldBackgroundColor: const Color(0xFFFFFFFF),
   snackBarTheme: const SnackBarThemeData(
