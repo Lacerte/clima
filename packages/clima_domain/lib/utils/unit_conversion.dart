97b47b07feefc04eb3d4b4cdcefe06fcff81b46f
mhmdanas <triallax@tutanota.com>
2022-01-21 20:56:00 +0300

    Use more accurate conversion factor

--- packages/clima_domain/lib/utils/unit_conversion.dart
+++ packages/clima_domain/lib/utils/unit_conversion.dart
@@ -2,6 +2,6 @@ double convertCelsiusToFahrenheit(double value) => 9 * value / 5 + 32;
 
 double convertFahrenheitToCelsius(double value) => 5 * (value - 32) / 9;
 
-double convertKilometersPerHourToMilesPerHour(double value) => value * 0.62137;
+double convertKilometersPerHourToMilesPerHour(double value) => value * 0.621371;
 
 double convertMilesPerHourToKilometersPerHour(double value) => value * 1.609344;
