6f0da23e65a9dace67e347438beeb413fe2839b7
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2022-01-22 14:52:48 +0300

    Add the MPL 2.0 header

--- packages/clima_domain/lib/utils/unit_conversion.dart
+++ packages/clima_domain/lib/utils/unit_conversion.dart
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 double convertCelsiusToFahrenheit(double value) => 9 * value / 5 + 32;
 
 double convertFahrenheitToCelsius(double value) => 5 * (value - 32) / 9;
