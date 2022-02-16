80fa0479ca0f21f44dd142db1955c53fe41fd689
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2022-02-16 16:35:32 +0300

    Update version number and changelog URL in about page (#259)

--- packages/clima_ui/lib/screens/about_screen.dart
+++ packages/clima_ui/lib/screens/about_screen.dart
@@ -43,13 +43,13 @@ class AboutScreen extends StatelessWidget {
             ),
             SettingsTile(
               title: 'Changelog',
-              subtitle: 'Version 2.0',
+              subtitle: 'Version 2.0.0',
               leading: Icon(
                 Icons.new_releases_outlined,
                 color: Theme.of(context).iconTheme.color,
               ),
               onTap: () => launch(
-                'https://github.com/lacerte/clima/tree/master/fastlane/metadata/android/en-US/changelogs',
+                'https://github.com/lacerte/clima/releases/tag/v2.0.0',
               ),
             ),
             SettingsTile(
