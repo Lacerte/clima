7a89d13c4bc7719f39388e6d0f8351172ab7d9b9
mhmdanas <triallax@tutanota.com>
2022-03-04 15:27:25 +0300

    [ui] make themes `final`

--- packages/clima_ui/lib/themes/black_theme.dart
+++ packages/clima_ui/lib/themes/black_theme.dart
@@ -9,7 +9,7 @@ import 'package:flutter/services.dart';
 
 import 'clima_theme.dart';
 
-ThemeData blackTheme = ThemeData.dark().copyWith(
+final blackTheme = ThemeData.dark().copyWith(
   // For some reason, the brightness seems to be wrong if it's not set
   // explicitly.
   brightness: Brightness.dark,
