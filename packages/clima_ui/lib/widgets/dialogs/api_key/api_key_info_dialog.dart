cc0ca27ae7b587a4bd351028460b5d58c919ea7a
PrestoSole <47897195+PrestoSole@users.noreply.github.com>
2022-06-11 15:35:03 +0300

    [clima_ui] add license header to missing files (#322)

--- packages/clima_ui/lib/widgets/dialogs/api_key/api_key_info_dialog.dart
+++ packages/clima_ui/lib/widgets/dialogs/api_key/api_key_info_dialog.dart
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 import 'package:clima_ui/widgets/others/link_text_span.dart';
 import 'package:flutter/material.dart';
 
